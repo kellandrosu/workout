e39d59d2532cf79b7a069839fbf718f671ab1f30
Andrius Kelly <kellandr@flip3.engr.oregonstate.edu>
2017-06-04 17:33:04 -0700

    first commit

---
+++ workouts.js
@@ -0,0 +1,96 @@
+//	Author: 	Andrius Kelly
+//  Dates: 		Jun  3 2017
+//	Assignment: CS290 Week 09 - Database interactions and UI 
+
+
+//modules
+var express = require("express");
+var handlebars = require("express-handlebars").create({defaultLayout: 'main'});
+var	bodyParser = require('body-parser');
+var mysql = require('mysql');
+
+var app = express();
+
+//set the port number
+app.set('port', 5121);
+
+//parse json and url
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//handlebars engine
+app.engine('handlebars', handlebars.engine); //apply h engine to h extensions
+app.set('view engine', 'handlebars'); //omit h extension for renders
+
+
+//MySQL connection pool
+var pool = mysql.createPool({
+  host  : 'localhost',
+  user  : 'student',
+  password: 'default',
+  database: 'student'
+});
+
+
+app.get('/', function(req,res,next){
+	var context = {};
+	pool.query('SELECT * FROM workouts', function(err, rows, fields){
+		if(err){
+			next(err);
+			return;
+		}
+		context.results = JSON.stringify(rows);
+		res.render('home', context);
+	});
+});
+
+//get request to /reset-table to create new table
+app.get('/reset-table',function(req,res,next){
+  var context = {};
+  pool.query("DROP TABLE IF EXISTS workouts", function(err){
+    var createString = "CREATE TABLE workouts("+
+    "id INT PRIMARY KEY AUTO_INCREMENT,"+
+    "name VARCHAR(255) NOT NULL,"+
+    "reps INT,"+
+    "weight INT,"+
+    "date DATE,"+
+    "lbs BOOLEAN)";
+    pool.query(createString, function(err){
+      context.results = "Table reset";
+      res.render('home',context);
+    })
+  });
+});
+
+
+//insert into database
+app.get('/insert', function(req,res,next){
+	var context = {};
+	pool.query("INSERT INTO workouts (`name`) VALUES (?)", [req.query.name], function(err, result){
+		if(err){
+			next(err);
+			return;
+		}
+		context.results = "Inserted id" + result.insertId;
+		res.render('home', context);
+	});
+});
+
+
+//404 catch all
+app.use(function(req,res){
+	res.status(404);
+	res.render("404");
+});
+
+//500 catch all
+app.use(function(e, req, res, next){
+	console.log(e.stack);
+//	res.type("plain/text");
+	res.status(500);
+	res.render("500");
+});
+
+app.listen( app.get('port'), function() {
+	console.log("Express started on http:flip3.engr.oregonstate.edu:" + app.get('port')+ " ; press Ctrl-C to terminate.");
+})
